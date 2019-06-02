9c817a9bf3298316a05a288f8db5aca1109e5a64
Sergey Plevako <sergey.plevako@corp.badoo.com>
2019-06-02 13:05:37 +0100

    added a stub chat storyboard

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -1,126 +1,38 @@
 //
-//  ViewController.swift
+//  ChatViewController.swift
 //  Flash Chat
 //
-//  Created by Angela Yu on 29/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//  Created by admin on 02/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
 import Firebase
-import ChameleonFramework
+import Chatto
+import ChattoAdditions
 
-class ChatViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
-    // Declare instance variables here
-    var messageArray : [Message] = [Message]()
+class ChatViewController: BaseChatViewController {
     
-    // We've pre-linked the IBOutlets
     @IBOutlet var heightConstraint: NSLayoutConstraint!
     @IBOutlet var sendButton: UIButton!
     @IBOutlet var messageTextfield: UITextField!
-    @IBOutlet var messageTableView: UITableView!
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //TODO: Set yourself as the delegate and datasource here:
-        messageTableView.delegate = self
-        messageTableView.dataSource = self
-        
-      
-        //TODO: Set yourself as the delegate of the text field here:
-        messageTextfield.delegate = self
-        
-        
-        //TODO: Set the tapGesture here:
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
-        messageTableView.addGestureRecognizer(tapGesture)
-        configureTableView()
-        retrieveMessages()
 
-        //TODO: Register your MessageCell.xib file here:
-        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
-        messageTableView.separatorStyle = .none
+        // Do any additional setup after loading the view.
     }
 
-    ///////////////////////////////////////////
-    
-    //MARK: - TableView DataSource Methods
-    
-    //TODO: Declare cellForRowAtIndexPath here:
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
-        //let messageArray = ["1st message", "2nd message", "3rd message"]
-        //cell.messageBody.text = messageArray[indexPath.row]
-        
-        cell.messageBody.text = messageArray[indexPath.row].messageBody
-        cell.senderUsername.text = messageArray[indexPath.row].sender
-        cell.avatarImageView.image = UIImage(named: "egg")
-        
-        if cell.senderUsername.text == Auth.auth().currentUser?.email as String! {
-            cell.avatarImageView.backgroundColor = UIColor.flatMint()
-            cell.messageBackground.backgroundColor = UIColor.flatSkyBlue()
-        } else {
-            cell.avatarImageView.backgroundColor = UIColor.flatWatermelon()
-            cell.messageBackground.backgroundColor = UIColor.flatGray()
-        }
-        
-        return cell
-    }
-    
-    //TODO: Declare numberOfRowsInSection here:
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return messageArray.count
-    }
-    
-    //TODO: Declare tableViewTapped here:
-    @objc func tableViewTapped() {
-        messageTextfield.endEditing(true)
-    }
-    
-    
-    //TODO: Declare configureTableView here:
-    func configureTableView() {
-        messageTableView.rowHeight = UITableView.automaticDimension
-        messageTableView.estimatedRowHeight = 120.0
-    }
-    
-    
-    ///////////////////////////////////////////
-    
-    //MARK:- TextField Delegate Methods
-    
-    //TODO: Declare textFieldDidBeginEditing here:
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        UIView.animate(withDuration: 0.5) {
-            self.heightConstraint.constant = 308 // 258 keyboard height + 50 message composer height
-            self.view.layoutIfNeeded()
-        }
-    }
-    
-    
-    
-    //TODO: Declare textFieldDidEndEditing here:
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        UIView.animate(withDuration: 0.5) {
-            self.heightConstraint.constant = 50 // 50 message composer height
-            self.view.layoutIfNeeded()
-        }
-    }
 
-    
-    ///////////////////////////////////////////
-    
-    
-    //MARK: - Send & Recieve from Firebase
-    
-    
-    
-    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
     
     @IBAction func sendPressed(_ sender: AnyObject) {
         
@@ -146,42 +58,5 @@ class ChatViewController: UIViewController, UITextFieldDelegate, UITableViewDele
             }
         }
     }
-    
-    //TODO: Create the retrieveMessages method here:
-    
-    func retrieveMessages() {
-        let messageDB = Database.database().reference().child("Messages")
-        messageDB.observe(.childAdded) { (snapshot) in
-            let snapshotValue = snapshot.value as! Dictionary<String,String>
-            let text = snapshotValue["MesssageBody"] ?? ""
-            let sender = snapshotValue["Sender"] ?? ""
-            
-            print(text, sender)
-            
-            let message = Message()
-            message.messageBody = text
-            message.sender = sender
-            self.messageArray.append(message)
-            self.configureTableView()
-            self.messageTableView.reloadData()
-        }
-    }
-
-    
-    
-    
-    @IBAction func logOutPressed(_ sender: AnyObject) {
-        
-        //TODO: Log out the user and send them back to WelcomeViewController
-        do {
-            try Auth.auth().signOut()
-            navigationController?.popToRootViewController(animated: true)
-        } catch  {
-            print("error: there was a problem logging out")
-        }
-        
-    }
-    
-
 
 }
